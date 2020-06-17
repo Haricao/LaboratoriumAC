67f43d0339c6171d924f7c6e14e0e157d8e53d20
Michał Gabryel <66183109+Haricao@users.noreply.github.com>
2020-06-17 12:45:45 +0200

    Add files via upload

--- projekt_jp_kkmr_v1.0/Properties/Settings.Designer.cs
+++ projekt_jp_kkmr_v1.0/Properties/Settings.Designer.cs
@@ -38,12 +38,12 @@ namespace projekt_jp_kkmr_v1._0.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("0")]
-        public double Prad {
+        public double Indukcyjnosc_L {
             get {
-                return ((double)(this["Prad"]));
+                return ((double)(this["Indukcyjnosc_L"]));
             }
             set {
-                this["Prad"] = value;
+                this["Indukcyjnosc_L"] = value;
             }
         }
         
@@ -61,13 +61,169 @@ namespace projekt_jp_kkmr_v1._0.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int Wybor {
+            get {
+                return ((int)(this["Wybor"]));
+            }
+            set {
+                this["Wybor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public double Frq {
             get {
                 return ((double)(this["Frq"]));
             }
             set {
                 this["Frq"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public double Pojemnosc {
+            get {
+                return ((double)(this["Pojemnosc"]));
+            }
+            set {
+                this["Pojemnosc"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Indukcyjnosc_C {
+            get {
+                return ((double)(this["Indukcyjnosc_C"]));
+            }
+            set {
+                this["Indukcyjnosc_C"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Napiecie_C {
+            get {
+                return ((double)(this["Napiecie_C"]));
+            }
+            set {
+                this["Napiecie_C"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Frq_C {
+            get {
+                return ((double)(this["Frq_C"]));
+            }
+            set {
+                this["Frq_C"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Pojemnosc_L {
+            get {
+                return ((double)(this["Pojemnosc_L"]));
+            }
+            set {
+                this["Pojemnosc_L"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Napiecie_L {
+            get {
+                return ((double)(this["Napiecie_L"]));
+            }
+            set {
+                this["Napiecie_L"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Frq_L {
+            get {
+                return ((double)(this["Frq_L"]));
+            }
+            set {
+                this["Frq_L"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Indukcyjnosc {
+            get {
+                return ((double)(this["Indukcyjnosc"]));
+            }
+            set {
+                this["Indukcyjnosc"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Uplywnosc {
+            get {
+                return ((double)(this["Uplywnosc"]));
+            }
+            set {
+                this["Uplywnosc"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Pojemnosc_C {
+            get {
+                return ((double)(this["Pojemnosc_C"]));
+            }
+            set {
+                this["Pojemnosc_C"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Rezystancja_C {
+            get {
+                return ((double)(this["Rezystancja_C"]));
+            }
+            set {
+                this["Rezystancja_C"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double Rezystancja_L {
+            get {
+                return ((double)(this["Rezystancja_L"]));
+            }
+            set {
+                this["Rezystancja_L"] = value;
             }
         }
     }
